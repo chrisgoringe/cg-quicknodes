88d33a24d37f66cd763727c58407080795a2305c
Chris <chris.goringe@gmail.com>
2023-10-15 15:43:28 +1100

    timer

---
+++ js/timer.js
@@ -0,0 +1,89 @@
+import { app } from "../../../scripts/app.js";
+import { ComfyWidgets } from "../../../scripts/widgets.js";
+
+class Timer {
+    static currentNodeId = null;
+    static lastChangeTime = null;
+    static start() {
+        Timer.display("\n--- Run Started ---\n");
+        const d = new Date();
+        const t = d.getTime();
+        Timer.lastChangeTime = t;
+        Timer.currentNodeId = app.runningNodeId;
+    }
+
+    static display(string) {
+        if (Timer.displayNode) {
+            Timer.displayNode.widgets[1].value += string;
+            Timer.displayNode.onResize?.(Timer.displayNode.size);
+        }
+    }
+
+    static clear() {
+        if (Timer.displayNode) {
+            Timer.displayNode.widgets[1].value = "";
+            Timer.displayNode.onResize?.(Timer.displayNode.size);
+        }
+    }
+
+    static tick() {
+        if (app.runningNodeId==Timer?.currentNodeId) return;
+
+        const d = new Date();
+        const t = d.getTime();
+        const nd = Timer.currentNodeId ? Timer.currentNodeId : 0;
+
+        if (!Timer.lastChangeTime) {
+            Timer.display("X")
+            Timer.lastChangeTime = t;
+            Timer.currentNodeId = app.runningNodeId;
+            return;
+        }
+
+        const dt = t - Timer.lastChangeTime;
+        const time = dt.toString().padStart(6);
+        const node = (nd>0) ? `node #${nd}` : "startup" ;
+        Timer.display(`${time} ms in ${node}\n`);
+
+        Timer.lastChangeTime = t;
+        Timer.currentNodeId = app.runningNodeId;
+
+        if (!Timer.currentNodeId) {
+            Timer.lastChangeTime = null;
+            Timer.display("---  Run Ended  ---\n");
+        }
+    }
+}
+
+app.registerExtension({
+	name: "cg.quicknodes.timer",
+    setup() {
+        const draw = LGraphCanvas.prototype.draw;
+        LGraphCanvas.prototype.draw = function() {
+            Timer.tick();
+            draw.apply(this,arguments);
+        }
+    },
+    async beforeRegisterNodeDef(nodeType, nodeData, app) {
+        if (nodeType.comfyClass=="Timer") {
+            const orig_executionStart = nodeType.prototype.onExecutionStart;
+            nodeType.prototype.onExecutionStart = function () {
+                orig_executionStart?.apply(this, arguments);
+                Timer.start();
+            }
+
+            const orig_nodeCreated = nodeType.prototype.onNodeCreated;
+            nodeType.prototype.onNodeCreated = function () {
+                orig_nodeCreated?.apply(this, arguments);
+                this.addWidget("button", "Clear", "", Timer.clear);
+                const w = ComfyWidgets["STRING"](this, "display_text_widget", ["STRING", { multiline: true }], app).widget;
+                w.inputEl.readOnly = true;
+                w.inputEl.style.opacity = 0.6;
+                w.inputEl.style.fontSize = "9pt";
+                w.value = "";
+                Timer.displayNode = this;
+            }
+        }
+    },
+
+})
---
+++ timer.py
@@ -0,0 +1,12 @@
+class Timer:
+    CATEGORY = "quicknodes"
+    @classmethod    
+    def INPUT_TYPES(s):
+        return { }
+    RETURN_TYPES = ()
+    RETURN_NAMES = ()
+    FUNCTION = "func"
+    def func(self):
+        return ()
+    
+CLAZZES = [Timer]
