2e697a3afbea5104d42eeaa28864e0b1d7aa7001
Chris <chris.goringe@gmail.com>
2024-07-23 17:27:18 +1000

    move class

--- image_size.py
+++ image_size.py
@@ -15,22 +15,6 @@ class ImageSize:
         b, h, w, c = image.shape
         PromptServer.instance.send_sync("cg.quicknodes.textmessage", {"id": node_id, "message":f"{w} x {h}"})
         return ()
-    
-class MaskToBlack:
-    CATEGORY = "quicknodes"
-    @classmethod
-    def INPUT_TYPES(s):
-       return {"required": { "image": ("IMAGE",), "mask": ("MASK",), "invert": (["no", "yes"], )} }
-    RETURN_TYPES = ( "IMAGE", )
-    FUNCTION = "func"
 
-    def func(self, image:torch.Tensor, mask:torch.Tensor, invert:str):
-        if len(mask.shape)==2: mask.unsqueeze_(0)
-        invert = (invert=="yes")
-        out_images = []
-        for img, msk in zip(image, mask):
-            if invert: msk = 1.0 - msk
-            out_images.append( torch.where(msk.unsqueeze(-1).expand((-1,-1,3))>0.5, torch.zeros_like(img), img) )
-        return (torch.stack( out_images, dim=0 ), )
 
-CLAZZES = [ ImageSize, MaskToBlack, ]
+CLAZZES = [ ImageSize, ]
--- mask.py
+++ mask.py
@@ -1,3 +1,5 @@
+import torch
+
 class InvertMaskOptional:
     @classmethod
     def INPUT_TYPES(cls):
@@ -10,4 +12,22 @@ class InvertMaskOptional:
     def func(self, mask, invert):
         return (1. - mask,) if invert=="yes" else (mask,)
     
-CLAZZES = [InvertMaskOptional]
+    
+class MaskToBlack:
+    CATEGORY = "quicknodes"
+    @classmethod
+    def INPUT_TYPES(s):
+       return {"required": { "image": ("IMAGE",), "mask": ("MASK",), "invert": (["no", "yes"], )} }
+    RETURN_TYPES = ( "IMAGE", )
+    FUNCTION = "func"
+
+    def func(self, image:torch.Tensor, mask:torch.Tensor, invert:str):
+        if len(mask.shape)==2: mask.unsqueeze_(0)
+        invert = (invert=="yes")
+        out_images = []
+        for img, msk in zip(image, mask):
+            if invert: msk = 1.0 - msk
+            out_images.append( torch.where(msk.unsqueeze(-1).expand((-1,-1,3))>0.5, torch.zeros_like(img), img) )
+        return (torch.stack( out_images, dim=0 ), )
+    
+CLAZZES = [InvertMaskOptional, MaskToBlack, ]
