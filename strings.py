be8fd424991a85b3d95f32f0f8a88df9de3b3e02
Chris <chris.goringe@gmail.com>
2024-07-30 09:20:34 +1000

    repalce

--- strings.py
+++ strings.py
@@ -12,4 +12,16 @@ class CombineStrings:
     def func(self, template:str, x="", y=""):
         return (template.replace("[X]",str(x)).replace("[Y]",str(y)),)
     
-CLAZZES = [CombineStrings,]
+class Substitute:
+    FUNCTION = "func"
+    CATEGORY = "quicknodes"
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {"string": ("STRING", {"default":"" }), "replace_": ("STRING", {"default":"" }), "with_": ("STRING", {"default":"" })}
+        }
+    RETURN_TYPES = ("STRING",)
+    def func(self, string:str, replace_, with_):
+        return (string.replace(replace_, with_),)
+    
+CLAZZES = [CombineStrings,Substitute]
