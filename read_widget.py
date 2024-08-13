31c68ce01ebb44515b2cf925b98d86e8df54ad9d
Chris <chris.goringe@gmail.com>
2024-08-14 08:52:58 +1000

    if absent

--- read_widget.py
+++ read_widget.py
@@ -2,21 +2,27 @@ class ReadWidget:
     CATEGORY = "quicknodes"
     @classmethod    
     def INPUT_TYPES(s):
-        return { "required":  { "node": ("INT", {"default":0}), "widget": ("STRING", {"default":""}), },
-                "hidden" : { "prompt":"PROMPT" } }
+        return { "required":  { 
+                    "node": ("INT", {"default":0}),
+                    "widget": ("STRING", {"default":""}), 
+                    "if_absent":("STRING",{"default":"not found"})
+                },
+                "hidden" : { 
+                    "prompt":"PROMPT" 
+                } }
     RETURN_TYPES = ("STRING",)
     RETURN_NAMES = ("text",)
     FUNCTION = "func"
-    def func(self, node, widget, prompt):
+    def func(self, node, widget, if_absent, prompt):
         try:
             w = prompt[str(node)]['inputs'][widget]
             w = f"{w}" if not isinstance(w,list) else f"{widget} in an input on #{node}"
         except:
-            w = f"{widget} not found on node #{node}"
+            w = if_absent
         return (w,)
     
     @classmethod
-    def IS_CHANGED(self, node, widget, prompt):
-        return self.func(node, widget, prompt)[0]
+    def IS_CHANGED(self, node, widget, if_absent, prompt):
+        return self.func(node, widget, if_absent, prompt)[0]
     
 CLAZZES = [ReadWidget]
