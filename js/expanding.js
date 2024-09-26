940b244113f81f30a74848b00bc78c68bdcf9e67
chris <chris.goringe@gmail.com>
2024-09-26 19:52:37 +1000

    fix expanding

--- js/expanding.js
+++ js/expanding.js
@@ -7,10 +7,13 @@ app.registerExtension({
         if (nodeType.comfyClass=="ExpandingMerge") {
             const onConnectionsChange = nodeType.prototype.onConnectionsChange;
             nodeType.prototype.onConnectionsChange = function (side,slot,connect,link_info,output) {     
+                console.log(arguments)
                 if (side==1) {
-                    const x = len(this.inputs) - 1
-                    this.addInput(`s${x}`, "STRING")
-                }   
+                    const last_slot = this.inputs.length - 1;
+                    if (slot==last_slot && link_info && last_slot<20) {
+                        this.addInput(`s${last_slot+1}`, "STRING")
+                    }
+                }  
             }
             return onConnectionsChange?.apply(this, arguments);
         }
--- xpanding.py
+++ xpanding.py
@@ -5,7 +5,7 @@ class ExpandingMerge:
     RETURN_TYPES = ("STRING",)
     OUTPUT_NODE = True
     @classmethod    
-    def INPUT_TYPES(s): return { "required":  { "link" : ("STRING",{"default":", "}), "s0" : ("STRING",{"default":""}) } }
+    def INPUT_TYPES(s): return { "required":  { "link" : ("STRING",{"default":", "}), "s0" : ("STRING",{"default":"", "force_input":True}) } }
     
     def func(self, link:str, **kwargs):
         return (link.join(kwargs[x] for x in kwargs), )
